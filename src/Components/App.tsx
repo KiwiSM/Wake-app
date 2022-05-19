db457dcd74254e6d56ee21846af762c1690164ae
Sovran Martet <sovran.martet@gmail.com>
2022-05-19 16:27:30 +0200

    Commit

---
+++ src/Components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+export default function App() {
+    return ( 
+        <article>
+            <h1>Haj</h1>
+            <ul>
+                <li>Angel Shark</li>
+                <li>Hammerhead Shark</li>
+                <li>Whale Shark</li>
+                <li>Great White Shark</li>
+                <li>Bull Shark</li>
+            </ul>
+        </article>
+    );
+}
